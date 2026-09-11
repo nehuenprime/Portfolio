a54a683ebd9321a3ac7743b9476adc25b31eeb60
Nehuen Boneo <nehuenprime@gmail.com>
2026-09-10 21:09:58 -0300

    cambiando todo el code actual

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const temaCache = localStorage.getItem("tema");
+const menuBtn = document.getElementById("menuBtn");
+const closeBtn = document.getElementById("closeBtn");
+const menuMobile = document.getElementById("menuMobile");
+const overlay = document.getElementById("overlay");
 
-if (temaCache === "claro") {
-    document.body.classList.add("modo-claro");
+function abrirMenu(){
+    menuMobile.classList.add("active");
+    overlay.classList.add("active");
 }
 
-const button = document.getElementById("btn-modo");
-
-if (button) {
-    button.addEventListener("click", function () {
-        document.body.classList.toggle("modo-claro");
-        if (document.body.classList.contains("modo-claro")) {
-            localStorage.setItem("tema", "oscuro");
-        } else {
-            localStorage.setItem("tema", "claro");
-        }
-    })
+function cerrarMenu(){
+    menuMobile.classList.remove("active");
+    overlay.classList.remove("active");
 }
+
+menuBtn.addEventListener("click", abrirMenu);
+closeBtn.addEventListener("click", cerrarMenu);
+overlay.addEventListener("click", cerrarMenu);
